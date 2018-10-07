aa868f89add2259afff057f0c705bbc2ee73da47
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2018-10-07 20:43:35 +0200

    Made in a general form

--- Backtracking/NQueens.cpp
+++ Backtracking/NQueens.cpp
@@ -1,58 +1,46 @@
-#include <bits/stdc++.h>
+#include <cstdio>
 
 using namespace std;
-const int N = 1e2 +5 , M = 1e4 +5, OO = 1000000007;
+const int N = 15 , M = 1e4 +5, OO = 1000000007;
 
 int n;
-bool r[N], md[2*N+5], sd[5*N+5];
-bool board[N][N];
+bool col[N], md[2*N], sd[2*N];
+char board[N][N];
 
-void printSolution(){
+void printGrid(){
+  puts("---------------------------------");
   for(int i = 0 ; i < n ; ++i){
     for(int j = 0; j < n ; ++j)
-      printf("%c", board[i][j]? 'Q' : '.');
+      printf("%c", board[i][j]);
     puts("");
   }
+  puts("---------------------------------");
 }
 
-bool valid(int x, int y){
-  return (!r[x] && !md[x-y+2*N] && !sd[x+y]);
+bool valid(int r, int c){
+  return (!col[c] && !md[r-c+N] && !sd[r+c]);
 }
 
-bool solveNQ(int col = 0){
-  if(col>=n)  return 1;
-  for(int row = 0 ; row < n ; ++row){
-    if(valid(row, col)){
-      r[row] = 1;
-      md[row-col+2*N] = 1;
-      sd[row+col] = 1;
-      board[row][col] = 1;
-      if(solveNQ(col+1))  return 1;
-      r[row] = 0;
-      md[row-col+2*N] = 0;
-      sd[row+col] = 0;
-      board[row][col] = 0;
+void solveNQ(int r = 0){
+  if(r == n){
+    printGrid();
+  }else{
+    for(int c = 0 ; c < n ; ++c){
+      if(valid(r, c)){
+        col[c] = md[r-c+N] = sd[r+c] = 1;     //do
+        board[r][c] = 'Q';                    //do
+        solveNQ(r+1);                         //recurse
+        col[c] = md[r-c+N] = sd[r+c] = 0;     //undo
+        board[r][c] = '.';                    //undo
+      }
     }
   }
-  return 0;
 }
 
 int main(){
-  // freopen("i.in", "rt", stdin);
-  // freopen("o.out", "wt", stdout);
   scanf("%d", &n);
-  if(solveNQ()) printSolution();
-  else  printf("No Solution\n");
+  for(int i = 0 ; i < n ; ++i)
+    for(int j = 0 ; j < n ; ++j)  board[i][j] = '.';
+  solveNQ();
   return 0;
 }
-
-
-
-
-
-
-
-
-
-/*
-*/

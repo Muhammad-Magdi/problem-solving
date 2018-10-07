d6d1fb8a60c899f0e860157d1ac3ebd9c66925ce
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2018-10-07 17:05:38 +0200

    Made in a general form

--- Backtracking/AllPermutations.cpp
+++ Backtracking/AllPermutations.cpp
@@ -1,44 +1,37 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <vector>
 
 using namespace std;
-const int N = 1e2 +5 , M = 1e4 +5, OO = 1000000007;
+const int N = 12 , M = 1e4 +5, OO = 0x3f3f3f3f;
 
-int n, A[N], permutation[N], sitted[N];
+int n, A[N], sitted[N];
+vector<int> permutation;
+
+void printPath(){
+  for(int i = 0 ; i < n ; ++i)
+    printf("%d%c", permutation[i], " \n"[i==n-1]);
+}
 
 void solveAP(int idx = 0){
   if(idx == n){
-    for(int i = 0 ; i < n ; ++i)
-      printf("%d ", permutation[i]);
-    puts("");
-    return;
-  }
-  for(int i = 0 ; i < n ; ++i){
-    if(!sitted[i]){
-      sitted[i] = 1;
-      permutation[idx] = A[i];
-      solveAP(idx+1);
-      sitted[i] = 0;
+    printPath();
+  }else{
+    for(int i = 0 ; i < n ; ++i){
+      if(!sitted[i]){
+        sitted[i] = 1;               //do
+        permutation.push_back(A[i]); //do
+        solveAP(idx+1);              //recurse
+        sitted[i] = 0;               //undo
+        permutation.pop_back();      //undo
+      }
     }
   }
 }
 
 int main(){
-  // freopen("i.in", "rt", stdin);
-  // freopen("o.out", "wt", stdout);
   scanf("%d", &n);
   for(int i = 0 ; i < n ; ++i)
     scanf("%d", A+i);
   solveAP();
   return 0;
 }
-
-
-
-
-
-
-
-
-
-/*
-*/

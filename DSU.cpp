32ce03baf0bbaf42b8341a0de0ca41c46986f549
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2018-10-07 21:31:58 +0200

    Edited DSU naming

--- DSU.cpp
+++ DSU.cpp
@@ -1,53 +1,43 @@
-#pragma GCC optimize ("O3")
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <numeric>
 
 using namespace std;
-const int N = (1<<17) , M = (1<<14), OO = 0x3f3f3f3f;
+const int N = 1e5+5 , M = (1<<14), OO = 0x3f3f3f3f;
 
 int n, q;
 int t, a, b;
 
 int parent[N];
+inline void init(){
+  iota(parent, parent+N, 0);
+}
 
 int findParent(int x){
 	if(parent[x] == x)	return x;
 	return parent[x] = findParent(parent[x]);
 }
 
-bool sameSet(int a, int b){
+inline bool sameSet(int a, int b){
 	return findParent(a) == findParent(b);
 }
 
-void unionSet(int a, int b){
-	int pa = findParent(a), pb = findParent(b);
-	if(pa == pb)	return;
-	parent[pb] = pa;
+inline void merge(int a, int b){
+	a = findParent(a), b = findParent(b);
+	if(a == b)	return;
+	parent[b] = a;
 }
 
-void init(){
-	for(int i = 0 ; i <= n ; ++i)	parent[i] = i;
-}
 
 int main(){
-  // freopen("i.in", "rt", stdin);
-  // freopen("o.out", "wt", stdout);
 	scanf("%d %d", &n, &q);
   init();
   while(q--){
   	scanf("%d %d %d", &t, &a, &b);
   	if(t){		//Make Friends
-  		unionSet(a, b);
+  		merge(a, b);
   	}else{		//Are Friends?
   		printf("%d\n", sameSet(a, b));
   	}
   }
 	return 0;
 }
-
-
-
-
-
-
-
-

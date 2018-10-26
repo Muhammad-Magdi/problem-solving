8f0ea54a2db25136cd0d56a198206498888b0f6f
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2018-10-26 16:20:40 +0200

    Added Memoization

--- Dynamic Programming/CoinChange.cpp
+++ Dynamic Programming/CoinChange.cpp
@@ -3,33 +3,20 @@
 using namespace std;
 const int N = (1<<20) , M = (1<<16), OO = 0x3f3f3f3f;
 
-int Coins[] = {5, 10, 25, 50, 100};
-long long mem[N][10];
 int n;
-
+int Coins[] = {5, 10, 25, 50, 100};
+long long mem[5][N];
 
 long long solve(int i = 0, int rem = n){			//O(n*n)
 	if(i==5)	return rem == 0;
-	return solve(i+1, rem) + (rem>=Coins[i]? solve(i, rem-Coins[i]) : 0);
-}
-
-/*
-long long coinChange(int rem = n, int startCoin = 0){						//O(n*n)
-	if(!rem)	return 1;
-	long long & ret = mem[rem][startCoin];
+	long long& ret = mem[i][rem];
 	if(~ret)	return ret;
-	ret = 0;
-	for(int coin = startCoin ; coin < 5 ; ++coin)
-		ret += (rem>=Coins[coin]? coinChange(rem-Coins[coin], coin) : 0);
-	return ret;
+	return ret = (rem>=Coins[i]? solve(i, rem-Coins[i]) : 0) + solve(i+1, rem);
 }
-*/
 
 int main(){
-  // freopen("i.in", "rt", stdin);
-  // freopen("o.out", "wt", stdout);
 	memset(mem, -1, sizeof mem);
 	scanf("%d", &n);
-	printf("%lld\n", coinChange());
+	printf("%lld\n", solve());
 	return 0;
 }
